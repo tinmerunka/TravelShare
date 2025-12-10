c55f4b3633f02cdd3f8c2638abac15d7ae1e4106
zupson <mzupan4@algebra.hr>
2025-12-11 00:02:12 +0100

    Add basic expense feature
    Added static example of expense on landing page.
    Implemented CRUD for expenses (Create, Delete, Details).
    Users can view, create, and delete expenses after login

---
+++ TravelShare/Models/Expenses/ExpenseShare.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelShare.Models.Expenses
+{
+    public class ExpenseShare
+    {
+        public int Id { get; set; }
+        public int ExpenseId { get; set; }
+        public int UserId { get; set; }
+        public double ShareAmount { get; set; }
+    }
+}
---
+++ TravelShare/ViewModels/ExpenseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using TravelShare.Models.Expenses;
+
+namespace TravelShare.ViewModels
+{
+    public class ExpenseViewModel
+    {
+        public int Id { get; set; }
+        public int TripId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Amount { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public List<string> PaidUsers { get; set; } = new();
+        public List<string> UnpaidUsers { get; set; } = new();
+
+        public List<ExpenseShareViewModel> Shares { get; set; } = new();
+
+    }
+}
